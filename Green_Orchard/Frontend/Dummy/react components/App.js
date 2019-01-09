2e126f49f5312c72833f01a6884b7a950396ee10
Sam Erickson <samerickson95@gmail.com>
2019-01-09 10:27:36 -0500

    adds skeletons for react files

---
+++ Green_Orchard/Frontend/Dummy/react components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import MonthPieChart from './PieChart.js';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.monthNames = {
+            "1": "January",
+            "2": "February",
+            "3": "March",
+            "4": "April",
+            "5": "May",
+            "6": "June",
+            "7": "July",
+            "8": "August",
+            "9": "September",
+            "10": "October",
+            "11": "November",
+            "12": "December"
+        };
+
+        this.state = {
+            month: '',
+            expenses: [],
+            categories: [],
+            categoricalData: []
+        }
+
+        this._handleClick = this._handleClick.bind(this);
+    }
+    
+    _handleClick(e) {
+        e.preventDefault();
+        console.dir(e);
+    }
+}
