ae3b74d1409360e160e25d6844df9850e75a20a1
Will Harris <Harriswill22@yahoo.com>
2019-01-10 09:19:59 -0500

    adds search bar and create react app content

---
+++ Green_Orchard/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/avery/DigitalCrafts/Capstone-Green_Orchard/Green_Orchard/Green_Orchard/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
