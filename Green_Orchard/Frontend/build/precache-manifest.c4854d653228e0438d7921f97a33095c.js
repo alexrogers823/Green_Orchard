2bc7fb8040dba9216d2eb678bf0b3a089acd5e1d
Alex Rogers <alex.rogers823@gmail.com>
2019-01-15 17:08:29 -0500

    added build folder

---
+++ Green_Orchard/Frontend/build/precache-manifest.c4854d653228e0438d7921f97a33095c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c491cae4c5c0d9c76977",
+    "url": "/static/js/main.c491cae4.chunk.js"
+  },
+  {
+    "revision": "26bb2a8b4841de8fa184",
+    "url": "/static/js/1.26bb2a8b.chunk.js"
+  },
+  {
+    "revision": "c491cae4c5c0d9c76977",
+    "url": "/static/css/main.a81d0177.chunk.css"
+  },
+  {
+    "revision": "4243b1b8d78769bc2406478715f37b97",
+    "url": "/index.html"
+  }
+];
