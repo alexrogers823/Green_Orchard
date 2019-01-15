5227b9cece5b7b0127d0d3cb0f43fdd5f977da00
Alex Rogers <alex.rogers823@gmail.com>
2019-01-15 14:57:52 -0500

    fully restructured project files before deployment

--- Green_Orchard/Frontend/summary-page-react/src/App.js
+++ Green_Orchard/Frontend/summary-page-react/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import config from './config';
+// import CreateBarChart from './bar-chart-example';
+import { BarChart } from 'react-d3-components';
 const NEWSAPIKEY = config.NEWSAPIKEY;
 console.log(NEWSAPIKEY);
 
 
 
 class App extends Component {
-    state = { 
+    state = {
     expenses: [
-        {"Expense":"mail","Cost":5.75,"Category":"Transport","Notes":"idk","Date":"Dec 12"},
-        {"Expense":"chipotle","Cost":6.97,"Category":"Res","Notes":"burrito bowl","Date":"Dec 12"},
-        {"Expense":"rent","Cost":800,"Category":"Rent","Notes":"clairmont reserve,","Date":"Dec 12"}
-],  
+        // {"Expense":"mail","Cost":5.75,"Category":"Transport","Notes":"idk","Date":"Dec 12"},
+        // {"Expense":"chipotle","Cost":6.97,"Category":"Res","Notes":"burrito bowl","Date":"Dec 12"},
+        // {"Expense":"rent","Cost":800,"Category":"Rent","Notes":"clairmont reserve,","Date":"Dec 12"}
+],
 
 news: [
     {}
@@ -22,7 +23,16 @@ news: [
     }
 
 
+
     componentDidMount(){
+      fetch('/expenses/summary/')
+        .then(r => r.json())
+        .then(({expenses}) => {
+          this.setState({
+            expenses,
+          })
+        })
+
         this.updateList();
         setInterval(this.updateList,1000);
     }
@@ -33,11 +43,11 @@ news: [
             this.setState({
                 news: cnbc.articles
             });
-            
+
         })
     }
 
-    render() { 
+    render() {
 const ListOfExpenses = this.state.expenses.map(expense =>{
     return(
         <div>
@@ -58,11 +68,35 @@ const CnbcNews = this.state.news.map(cnbcnews =>{
             <p>{cnbcnews.publishedAt}</p>
             <a target="_blank" href={cnbcnews.url}>{cnbcnews.title}</a>
             </li>
-            
+
         </ul>
         </div>
     )
 })
+
+const CreateBarChart = () => {
+  const SummaryBarChart = BarChart;
+  const data = [{
+    label: 'somethingA',
+    values: [
+      {x: 'Rent', y: 10},
+      {x: 'Utilities', y: 4},
+      {x: 'Groceries', y: 3},
+      {x: 'Supplies', y: 6},
+      {x: 'Apparel', y: 8},
+      {x: 'Education', y: 4},
+      {x: 'Other', y: 3}
+    ]
+  }];
+
+  return (
+    <SummaryBarChart
+    data={data}
+    width={400}
+    height={400}
+    margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
+  );
+}
 
 
         return (
@@ -72,7 +106,7 @@ const CnbcNews = this.state.news.map(cnbcnews =>{
         <h2>Take a Look at your Orchard</h2>
     </div>
 
-    
+
 
     <div class="Top5">
 
@@ -80,16 +114,16 @@ const CnbcNews = this.state.news.map(cnbcnews =>{
             <h3>All Expenses</h3>
             <ul>
                 <li>{ListOfExpenses}</li>
-                
-          
+
             </ul>
         </div>
 
         <div class="grouped">
             <h3>Grouped Expenses</h3>
             <h1>Bar Chart</h1>
+            <div>{CreateBarChart}</div>
         </div>
-            
+
         <div class="News">
             <h3>News</h3>
             <div class="CNBC">
@@ -99,7 +133,7 @@ const CnbcNews = this.state.news.map(cnbcnews =>{
             </div>
         </div>
 
-    </div> 
+    </div>
     </div>
     );
 
@@ -107,5 +141,3 @@ const CnbcNews = this.state.news.map(cnbcnews =>{
 }
 
 export default App ;
-
-
